79d37a1f51a357b1d8c8dc4a00e66106d492d920
aryyyyy31 <aryadevitra97@gamil.com>
2023-09-29 09:35:48 +0700

    dashboard

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
 Route::post('/', [App\Http\Controllers\HomeController::class, 'store'])->name('index.store');
 
