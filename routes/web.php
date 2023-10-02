fd5f2f6ede135f7f5683d2729571ff2beb8557bb
kahfinurfajar28 <kfiiinrfjr@gmail.com>
2023-10-02 09:38:57 +0700

    landingpage

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
-Route::post('/', [App\Http\Controllers\HomeController::class, 'store'])->name('index.store');
+
+Route::get('/home.index', [App\Http\Controllers\HomeController::class, 'index'])->name('index');
+Route::post('/home.index', [App\Http\Controllers\HomeController::class, 'store'])->name('index.store');
 
 Auth::routes();
 
